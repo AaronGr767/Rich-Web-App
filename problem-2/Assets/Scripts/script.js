1b1b5c4c833da1cc0dcd8e15af672e891e9bfcf2
AaronGr767 <aarongroarke2@gmail.com>
2022-10-10 12:36:14 +0100

    Improved code format for readability

--- problem-2/Assets/Scripts/script.js
+++ problem-2/Assets/Scripts/script.js
@@ -1,7 +1,7 @@
 let formVisibilty = document.getElementById('NoteCreationPopUp');
-    formVisibilty.style.display = 'none';
+formVisibilty.style.display = 'none';
 
-    document.querySelector("form").reset();
+document.querySelector("form").reset();
 
 showNotes();
 
@@ -9,13 +9,13 @@ let create_btn = document.getElementById('create-note-button');
 let add_btn = document.getElementById('add-note-button');
 
 create_btn.addEventListener('click', () => {
-  let formVisibilty = document.getElementById('NoteCreationPopUp');
+    let formVisibilty = document.getElementById('NoteCreationPopUp');
 
-  if (formVisibilty.style.display == 'none') {
-    formVisibilty.style.display = 'block';
-  } else {
-    formVisibilty.style.display = 'none';
-  }
+    if (formVisibilty.style.display == 'none') {
+        formVisibilty.style.display = 'block';
+    } else {
+        formVisibilty.style.display = 'none';
+    }
 });
 
 
@@ -43,7 +43,6 @@ add_btn.addEventListener('click', () => {
     
     noteIndex.push(createdNote);
     localStorage.setItem('notesStore', JSON.stringify(noteIndex));
-
     
     document.querySelector("form").reset();
 
@@ -61,26 +60,25 @@ function showNotes() {
       } else {
         noteIndex = JSON.parse(notes);
     }
+
     let html = "";
     
     noteIndex.forEach(function(element, index) {
         html += ` <div class="DisplayedNote" style="background-color: ${element.noteColours}">
-        <div class="DisplayedNoteContent">
-         <p class="NameText">${element.noteName}</p>
-         <p class="ContentText">${element.noteContent}</p>
-         </div>
- 
-        <button id="${index}"onclick="editNote(this.id)" class="edit-note-button">Edit Note</button>
-		<button id="${index}"onclick="deleteNote(this.id)" class="delete-note-button">Delete Note</button>
-	</div>`;
+                    <div class="DisplayedNoteContent">
+                        <p class="NameText">${element.noteName}</p>
+                        <p class="ContentText">${element.noteContent}</p>
+                    </div>
+                    <button id="${index}"onclick="editNote(this.id)" class="edit-note-button">Edit Note</button>
+		            <button id="${index}"onclick="deleteNote(this.id)" class="delete-note-button">Delete Note</button>
+	            </div>`;
     });
     
-    let notesElement = document.getElementById("DisplayedNotesArea");
-    notesElement.innerHTML = html;
+    let noteHTML = document.getElementById("DisplayedNotesArea");
+    noteHTML.innerHTML = html;
+}
 
-  }
-
-  function editNote(index) {
+function editNote(index) {
     let formVisibilty = document.getElementById('NoteCreationPopUp');
     formVisibilty.style.display = 'block';
 
@@ -98,21 +96,20 @@ function showNotes() {
     content.value = noteIndex[index].noteContent;
     colours.value = noteIndex[index].noteColours;
 
-
     noteIndex.splice(index, 1);
     localStorage.setItem("notesStore", JSON.stringify(noteIndex));
     showNotes();
-  }
+}
 
-  function deleteNote(index) {
-    
-        if (localStorage.getItem("notesStore") == null) {
-            noteIndex = [];
-        }else{
-            noteIndex = JSON.parse(localStorage.getItem('notesStore'));
-        }
-      noteIndex.splice(index, 1);
-      localStorage.setItem("notesStore", JSON.stringify(noteIndex));
-      showNotes();
+function deleteNote(index) {
+    if (localStorage.getItem("notesStore") == null) {
+        noteIndex = [];
+    }else{
+        noteIndex = JSON.parse(localStorage.getItem('notesStore'));
     }
+      
+    noteIndex.splice(index, 1);
+    localStorage.setItem("notesStore", JSON.stringify(noteIndex));
+    showNotes();
+}
   
