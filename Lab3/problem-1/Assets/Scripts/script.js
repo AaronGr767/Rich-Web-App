08d3b40e28f567cbe366ff6f95e5f02a4e85b860
AaronGr767 <aarongroarke2@gmail.com>
2022-10-23 02:00:35 +0100

    Added appropriate comments for JS

--- Lab3/problem-1/Assets/Scripts/script.js
+++ Lab3/problem-1/Assets/Scripts/script.js
@@ -6,6 +6,7 @@ document.getElementById('formName').value = "";
 document.getElementById('formNumber').value = "";
 document.getElementById('formEmail').value = "";
 
+//When user attempts to add contact, enter this function
 add_btn.addEventListener('click', () => {
     let fName = document.getElementById('formName');
     let fNumber = document.getElementById('formNumber');
@@ -15,7 +16,7 @@ add_btn.addEventListener('click', () => {
     var phoneTest = /^\d{10}$/;
     var emailTest = /^(?!.{40})[a-z0-9]+(?:\.[a-z0-9]+)*@[a-z0-9-]+(?:\.[a-z0-9-]+)*\.[a-zA-Z]{2,6}$/;
     
-     if (!fName.value.match(nameTest)) {
+     if (!fName.value.match(nameTest)) { //Validates name by comparing with suitable regex
 
             showErrorMessage();
 
@@ -25,7 +26,7 @@ add_btn.addEventListener('click', () => {
                     </div>`;
             document.getElementById('errorPlaceholder').innerHTML = html;
 
-            } else if(!fNumber.value.match(phoneTest)) {
+            } else if(!fNumber.value.match(phoneTest)) { //Validates mobile number by comparing with suitable regex
 
                 showErrorMessage();
 
@@ -35,7 +36,7 @@ add_btn.addEventListener('click', () => {
                         </div>`;
                 document.getElementById('errorPlaceholder').innerHTML = html;
                 
-            }  else if(!fEmail.value.match(emailTest)) {
+            }  else if(!fEmail.value.match(emailTest)) { //Validates email by comparing with suitable regex
 
                 showErrorMessage();
 
@@ -45,17 +46,19 @@ add_btn.addEventListener('click', () => {
                         </div>`;
                 document.getElementById('errorPlaceholder').innerHTML = html;
             } else {
-                addContact();
+                addContact(); //If validation passed then procedd to function to add contact
             }
 
 });
 
+//Hides pop up error message for contact info validation
 function hideErrorMessage(){
     let errorHTML = document.getElementById('errorPlaceholder');
         errorHTML.style.display = 'none';
         errorHTML.style.visibility = 'hidden';
 };
 
+//Shows pop up error message for contact info validation
 function showErrorMessage(){
     let errorHTML = document.getElementById('errorPlaceholder');
         errorHTML.style.display = 'block';
@@ -64,6 +67,7 @@ function showErrorMessage(){
         return errorHTML;
 };
 
+//Adds contact info to local storage
 function addContact(){
     let fName = document.getElementById('formName');
     let fNumber = document.getElementById('formNumber');
@@ -72,7 +76,7 @@ function addContact(){
     let contacts = localStorage.getItem("contactStore");
 
 
-    if (contacts == null) {
+    if (contacts == null) {//checks to see if local storage is empty
         contactsIndex = [];
     }else{
         contactsIndex = JSON.parse(contacts);
@@ -88,13 +92,14 @@ function addContact(){
     contactsIndex.push(savedContact);
     localStorage.setItem('contactStore', JSON.stringify(contactsIndex));
 
-    showContacts();
+    showContacts();//Refreshes show contacts so new addition is displayed
 }
 
+//Displays all added contacts present in local storage
 function showContacts() {
     let contacts = localStorage.getItem("contactStore");
 
-    if (contacts == null) {
+    if (contacts == null) {//checks to see if local storage is empty
         contactsIndex = [];
       } else {
         contactsIndex = JSON.parse(contacts);
@@ -102,7 +107,7 @@ function showContacts() {
 
     let html = "";
     
-    contactsIndex.forEach(function(element, index) {
+    contactsIndex.forEach(function(element, index) { //Cycles through each entry and displays it in table
         html += `<tr>
                     <td>${element.formName}</td>
                     <td>${element.formNumber}</td>
@@ -114,39 +119,42 @@ function showContacts() {
     contactHTML.innerHTML = html;
 };
 
+//Sorts table by name alphabetically (ascending & descending)
 function sortAlpha(){
         var contacts, rows, switching, i, comp1, comp2, switched;
         contacts = document.getElementById("contactsTable");
         switching = true;
-        while (switching) {
+        while (switching) { //Loops until no more switching to be done
           switching = false;
           rows = contacts.rows;
 
-          for (i = 1; i < (rows.length - 1); i++) {
+          for (i = 1; i < (rows.length - 1); i++) { //Iterates through each row for comparison
             switched = false;
             comp1 = rows[i].getElementsByTagName("TD")[0];
             comp2 = rows[i + 1].getElementsByTagName("TD")[0];
-            if (alreadyClicked == false){
+
+            if (alreadyClicked == false){ //Checks to order in descending order
                 if (comp1.innerHTML.toLowerCase() > comp2.innerHTML.toLowerCase()) {
                 switched = true;
                 break;
                 }
-            } else if(alreadyClicked == true){
+            } else if(alreadyClicked == true){ //Checks to order in ascending order
                 if (comp1.innerHTML.toLowerCase() < comp2.innerHTML.toLowerCase()) {
                     switched = true;
                     break;
                 }
             }
           }
-          if (switched) {
+          if (switched) { //If switch needs to be made, make switch
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
-          } else{
-            alreadyClicked = !alreadyClicked;
+          } else{ //If no switch needs to be made then toggle the check for descending/ascending order
+            alreadyClicked = !alreadyClicked; 
           }
         }
 }
 
+//Searches table based on mobile number input
 function searchNumber(){
     var searchCount = 0;
 
@@ -154,14 +162,14 @@ function searchNumber(){
     var conTable = document.getElementById("contactsTable");
     var ctRows = conTable.getElementsByTagName("tr");
   
-    for (i = 0; i < ctRows.length; i++) {
+    for (i = 0; i < ctRows.length; i++) { //Iterates through each row
         tData = ctRows[i].getElementsByTagName("td")[1];
-      if (tData) {
+      if (tData) { //If there is any data in the cell
         numValue = tData.textContent || tData.innerText;
-        if (numValue.indexOf(searchValue.value) > -1) {
+        if (numValue.indexOf(searchValue.value) > -1) { //If search input is present in the row then dont hide it
             ctRows[i].style.display = "";
-            searchCount ++;
-        } else {
+            searchCount ++; 
+        } else { //If not present in the row then hide the row
             ctRows[i].style.display = "none";
         }
       }
@@ -169,10 +177,10 @@ function searchNumber(){
 
     let noRes = document.getElementById('noResult');
 
-    if(searchCount == 0){
+    if(searchCount == 0){ //If no match found then display appropriate message
         noRes.style.display = 'block';
         noRes.style.visibility = 'visible';
-    } else{
+    } else{ //Otherwise hide the message
         noRes.style.display = 'none';
         noRes.style.visibility = 'hidden';
     }
