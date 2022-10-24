9756c3816355559c6d92c853fa8b6d6d3cd61ed1
AaronGr767 <aarongroarke2@gmail.com>
2022-10-24 13:16:44 +0100

    Implemented basic frequency map for number of words in body

--- Lab3/problem-2/script.mjs
+++ Lab3/problem-2/script.mjs
@@ -1,13 +1,24 @@
 import fetch from "node-fetch";
+let counter = 0;
 
 fetch('https://jsonplaceholder.typicode.com/posts/')
   .then(response => response.json())
-  .then(posts => console.log(posts.filter(posts => countPostTitle(posts.title))))
+  //.then(posts => console.log(posts.filter(posts => countPostTitle(posts.title))))
+  .then(posts => console.log(posts.map(posts => bodyMap(posts.body))))
 
-  // console.log(posts);
 
   function countPostTitle(title){
     const arr = title.split(' ');
 
     return arr.filter(word => word !== '').length > 6;
+  }
+
+  function bodyMap(body){
+    const arr = body.split(' ');
+    var bodyArray= [];
+    counter++;
+
+    bodyArray[counter] = arr.filter(word => word !== '').length 
+
+    return bodyArray;
   }
