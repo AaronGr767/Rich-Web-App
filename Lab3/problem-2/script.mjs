4ef7c6c94a0f5a852290f4114a4bd57107173e74
AaronGr767 <aarongroarke2@gmail.com>
2022-10-24 10:51:04 +0100

    Implemented function for filtering by number of words in post title

--- Lab3/problem-2/script.mjs
+++ Lab3/problem-2/script.mjs
@@ -2,4 +2,12 @@ import fetch from "node-fetch";
 
 fetch('https://jsonplaceholder.typicode.com/posts/')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(posts => console.log(posts.filter(posts => countPostTitle(posts.title))))
+
+  // console.log(posts);
+
+  function countPostTitle(title){
+    const arr = title.split(' ');
+
+    return arr.filter(word => word !== '').length > 6;
+  }
