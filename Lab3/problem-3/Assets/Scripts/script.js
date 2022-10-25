056aafbba71c392f3c0bd185de26449afcafcd1e
AaronGr767 <aarongroarke2@gmail.com>
2022-10-25 15:03:02 +0100

    Populated page with user search data

--- Lab3/problem-3/Assets/Scripts/script.js
+++ Lab3/problem-3/Assets/Scripts/script.js
@@ -7,8 +7,33 @@ search_btn.addEventListener('click', () => {
     if(searchName.value.length == 0){
         alert('You must enter a username first!');
     }
+     
+    let prPicture = document.getElementById('profilePicture');
+    let prName = document.getElementById('profileName');
+    let prUsername = document.getElementById('profileUsername');
+    let prEmail = document.getElementById('profileEmail');
+    let prLocation = document.getElementById('profileLocation');
+    let prGists = document.getElementById('profileGists');
 
     let userRequest = fetch(`https://api.github.com/users/${searchName.value}`);
 
-    userRequest.then(response => console.log(response.json()));
+    userRequest.then(response => response.json())
+    .then(userInfo => {
+
+        console.log(userInfo);
+
+        prPicture.src = userInfo.avatar_url;
+        prName.innerHTML = userInfo.name;
+        prUsername.innerHTML = userInfo.login;
+        prEmail.innerHTML = userInfo.email;
+        prLocation.innerHTML = userInfo.location;
+        prGists.innerHTML = userInfo.public_gists
+    })
+
+
 });
+
+function showUserSearch(){
+    let
+
+}
