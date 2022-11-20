a1ca4397083ae2ad5454ba16d4e97bb26e861d7d
AaronGr767 <aarongroarke2@gmail.com>
2022-11-20 23:15:58 +0000

    Created blank observable function for start button

--- Lab4/problem-2/Assets/Scripts/script.js
+++ Lab4/problem-2/Assets/Scripts/script.js
@@ -1,2 +1,9 @@
 const { fromEvent } = rxjs;
 
+const start_btn = document.getElementById('startButton');
+
+const observeStart = fromEvent(start_btn, 'click');
+
+const subscriptionStart = observeStart.subscribe(event => {
+    subscriptionStart.unsubscribe();
+});
