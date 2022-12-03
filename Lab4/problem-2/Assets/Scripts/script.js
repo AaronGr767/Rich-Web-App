232a902a51adf3f8b2cd22935734e2af1a8fbe70
AaronGr767 <aarongroarke2@gmail.com>
2022-12-03 18:01:45 +0000

    Implemented timer function

--- Lab4/problem-2/Assets/Scripts/script.js
+++ Lab4/problem-2/Assets/Scripts/script.js
@@ -8,7 +8,7 @@ document.getElementById('minuteText').value = "";
 document.getElementById('hourText').value= "";
 
 const start_btn = document.getElementById('startButton');
-let countHTML = document.getElementById('countDownBox')
+let countHTML = document.getElementById('countdownBox')
 
 
 fromEvent(start_btn, 'click').subscribe(() => {
@@ -16,19 +16,43 @@ fromEvent(start_btn, 'click').subscribe(() => {
     let inputMin = (parseInt(document.getElementById('minuteText').value));
     let inputHour = (parseInt(document.getElementById('hourText').value));
 
-    let timerSec = inputSec * 1000;
-    let timerMin = inputMin * 1000 * 60;
-    let timerHour = inputHour * 1000 * 60 * 60;
 
     if(!inputSec && !inputMin && !inputHour ){
         alert("Timer will not start until user enters valid numbers!");
     }
-    console.log(timerSec + " + " + timerMin + " + " + timerHour);
+    else{
+        let timerSec = inputSec;
+        let timerMin = inputMin* 60;
+        let timerHour = inputHour* 60 * 60;
 
-    
-
+        let total_time = timerSec + timerMin + timerHour;
+        countdown(total_time);
+    }
+    // console.log(timerSec + " + " + timerMin + " + " + timerHour);
 
 });
+
+const displayTimeUnits = (time) => ({
+    hours: Math.floor(time / 3600),
+    minutes: Math.floor((time % 3600 / 60)),
+    seconds: Math.floor(time % 3600 % 60)
+});
+
+function countdown(timer) {
+    
+    const time = interval(1000); 
+
+        time
+        .pipe(take(timer))
+        .pipe(map((val) => (timer - 1) - val)) 
+        .pipe(map(displayTimeUnits)) 
+        .subscribe((time) => {
+            let html = "";
+            html = `<div id="countdown">${time.hours} : ${time.minutes} : ${time.seconds}</div>`
+            countHTML.innerHTML = html;
+        });
+
+}
 
 // let html = "";
 
