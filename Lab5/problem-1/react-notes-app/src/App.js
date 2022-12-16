00fffccefc7377d550fadc270a0bde706ece80a6
AaronGr767 <aarongroarke2@gmail.com>
2022-12-16 21:12:44 +0000

    Added constant header to site

--- Lab5/problem-1/react-notes-app/src/App.js
+++ Lab5/problem-1/react-notes-app/src/App.js
@@ -1,26 +1,35 @@
 import NotesList from "./components/NotesList";
 import {useState} from 'react';
 import {nanoid} from 'nanoid';
+import AddNote from "./components/AddNote";
 
 const App = () => {
-  const [notes, setNotes] = useState([{
-    id: nanoid(),
-    name: "Example1",
-    desc: "Hello There!"
-  },{
-    id: nanoid(),
-    name: "Example2",
-    desc: "Hello There Again!"
-  },{
-    id: nanoid(),
-    name: "Example3",
-    desc: "Hello There Once Again!"
-  }])
+  const [notes, setNotes] = useState([])
+
+  const[visible,setisvisible] = useState(false);
+
+  const create_note = () => {console.log("Create note")}
+  
+  const addNote = () => {
+    console.log("adding note!")
+  }
 
   return (
+    <>
+    <div>
+    <div class="Wrapper">
+        <h1>Problem 1 - Note Taking App in React JS</h1>
+        
+            <button id="create-note-button" onClick={create_note}>Click here to create a note!</button>
+    </div>
+    </div>
+
+    <AddNote/>    
+
     <div className="container">
       <NotesList notes={notes}/>
     </div>
+    </>
   );
 };
 
