b7754c1b2e1e8466b30e458c9aa3a7c6af2a11ed
AaronGr767 <aarongroarke2@gmail.com>
2022-12-16 23:16:29 +0000

    Updated notelist

--- Lab5/problem-1/react-notes-app/src/components/NotesList.js
+++ Lab5/problem-1/react-notes-app/src/components/NotesList.js
@@ -1,9 +1,9 @@
 import Note from './Note'
 
-const NotesList = ({notes}) => {
+const NotesList = ({notes, handleDeleteNote, handleEditNote}) => {
     return(
         <div className="notes-list">
-            {notes.map((note)=> (<Note id={note.id} name={note.name} desc={note.desc}/>))}
+            {notes.map((note)=> (<Note id={note.id} name={note.name} desc={note.desc} handleDeleteNote={handleDeleteNote} handleEditNote ={handleEditNote}/>))}
         </div>
     )
 }
